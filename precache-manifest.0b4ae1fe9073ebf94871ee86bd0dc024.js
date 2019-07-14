9ca86fbb50727d3ec4f6b75d76266d01d7348bb9
DonteGitsIt <donte.ojeda@gmail.com>
2019-07-13 22:40:56 -0400

    Updates

---
+++ precache-manifest.0b4ae1fe9073ebf94871ee86bd0dc024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290159cf45aa776b7c42e940cfb61dc6",
+    "url": "/screening-project/index.html"
+  },
+  {
+    "revision": "4166e8835c3569e3bb79",
+    "url": "/screening-project/static/css/main.128bde02.chunk.css"
+  },
+  {
+    "revision": "c64d78b97377a70ce5aa",
+    "url": "/screening-project/static/js/2.f530fa2c.chunk.js"
+  },
+  {
+    "revision": "4166e8835c3569e3bb79",
+    "url": "/screening-project/static/js/main.82b6cd59.chunk.js"
+  },
+  {
+    "revision": "0a3582b72c055f0d6395",
+    "url": "/screening-project/static/js/runtime~main.762d052c.js"
+  }
+]);
